c55d8f9e08b771fc11c9c26c712c2119f01718d3
Linh Tran <linh.mtran168@live.com>
2013-03-15 15:39:37 +0700

    change select options when default date was set + fix some lint error

--- bday-picker.js
+++ bday-picker.js
@@ -7,7 +7,7 @@
  *
  */
 
-(function( $ ){
+(function($){
 
   // plugin variables
   var months = {
@@ -38,6 +38,9 @@
       "tabindex"      : null
     };
 
+
+
+    // Return
     return this.each(function() {
 
       if (options) { $.extend(settings, options); }
@@ -48,80 +51,8 @@
           $month = $("<select class='birth-month' name='birth[month]'></select>"),
           $day = $("<select class='birth-day' name='birth[day]'></select>");
 
-      if (settings["legend"]) { $("<legend>" + settings["legend"] + "</legend>").appendTo($fieldset); }
-
-      var tabindex = settings["tabindex"];
-
-      // Deal with the various Date Formats
-      if (settings["dateFormat"] == "bigEndian") {
-        $fieldset.append($year).append($month).append($day);
-        if (tabindex != null) {
-          $year.attr('tabindex', tabindex);
-          $month.attr('tabindex', tabindex++);
-          $day.attr('tabindex', tabindex++);
-        }
-      } else if (settings["dateFormat"] == "littleEndian") {
-        $fieldset.append($day).append($month).append($year);
-        if (tabindex != null) {
-          $day.attr('tabindex', tabindex);
-          $month.attr('tabindex', tabindex++);
-          $year.attr('tabindex', tabindex++);
-        }
-      } else {
-        $fieldset.append($month).append($day).append($year);
-        if (tabindex != null) {
-          $month.attr('tabindex', tabindex);
-          $day.attr('tabindex', tabindex++);
-          $year.attr('tabindex', tabindex++);
-        }
-      }
-
-      // Add the option placeholders if specified
-      if (settings["placeholder"]) {
-        $("<option value='0'>Year:</option>").appendTo($year);
-        $("<option value='0'>Month:</option>").appendTo($month);
-        $("<option value='0'>Day:</option>").appendTo($day);
-      }
-
-      var hiddenDate;
-      if (settings["defaultDate"]) {
-        var defDate = new Date(settings["defaultDate"] + "T00:00:00"),
-        defYear = defDate.getFullYear(),
-        defMonth = defDate.getMonth() + 1,
-        defDay = defDate.getDate();
-        hiddenDate = defYear + "-" + defMonth + "-" + defDay;
-      }
-
-      // Create the hidden date markup
-      if (settings["hiddenDate"]) {
-        $("<input type='hidden' name='" + settings["fieldName"] + "'/>")
-            .attr("id", settings["fieldId"])
-            .val(hiddenDate)
-            .appendTo($fieldset);
-      }
-
-      // Build the initial option sets
-      var startYear = todayYear - settings["minAge"];
-      var endYear = todayYear - settings["maxAge"];
-      if (settings["futureDates"] && settings["maxYear"] != todayYear) {
-        if (settings["maxYear"] > 1000) { startYear = settings["maxYear"]; }
-        else { startYear = todayYear + settings["maxYear"]; }
-      }
-      for (var i=startYear; i>=endYear; i--) { $("<option></option>").attr("value", i).text(i).appendTo($year); }
-      for (var j=0; j<12; j++) { $("<option></option>").attr("value", j+1).text(months[settings["monthFormat"]][j]).appendTo($month); }
-      for (var k=1; k<32; k++) { $("<option></option>").attr("value", k).text(k).appendTo($day); }
-      $(this).append($fieldset);
-
-      // Set the default date if given
-      if (settings["defaultDate"]) {
-        var date = new Date(settings["defaultDate"] + "T00:00:00");
-        $year.val(date.getFullYear());
-        $month.val(date.getMonth() + 1);
-        $day.val(date.getDate());
-      }
-
-      // Update the option sets according to options and user selections
-      $fieldset.change(function() {
+      // Funtion to update the valude of select boxes
+      var updateSelectOptions = function() {
             // todays date values
         var todayDate = new Date(),
             todayYear = todayDate.getFullYear(),
@@ -134,8 +65,8 @@
             // number of days in currently selected year/month
             actMaxDay = (new Date(selectedYear, selectedMonth, 0)).getDate(),
             // max values currently in the markup
-            curMaxMonth = parseInt($month.children(":last").val()),
-            curMaxDay = parseInt($day.children(":last").val());
+            curMaxMonth = parseInt($month.children(":last").val(), 10),
+            curMaxDay = parseInt($day.children(":last").val(), 10);
 
         // Dealing with the number of days in a month
         // http://bugs.jquery.com/ticket/3041
@@ -153,7 +84,7 @@
 
         // Dealing with future months/days in current year
         // or months/days that fall after the minimum age
-        if (!settings["futureDates"] && selectedYear == startYear) {
+        if (!settings.futureDates && selectedYear === startYear) {
           if (curMaxMonth > todayMonth) {
             while (curMaxMonth > todayMonth) {
               $month.children(":last").remove();
@@ -172,22 +103,105 @@
 
         // Adding months back that may have been removed
         // http://bugs.jquery.com/ticket/3041
-        if (selectedYear != startYear && curMaxMonth != 12) {
+        if (selectedYear !== startYear && curMaxMonth !== 12) {
           while (curMaxMonth < 12) {
-            $month.append("<option value=" + (curMaxMonth+1) + ">" + months[settings["monthFormat"]][curMaxMonth] + "</option>");
+            $month.append("<option value=" + (curMaxMonth+1) + ">" + months[settings.monthFormat][curMaxMonth] + "</option>");
             curMaxMonth++;
           }
         }
 
         // update the hidden date
-        if ((selectedYear * selectedMonth * selectedDay) != 0) {
+        if ((selectedYear * selectedMonth * selectedDay) !== 0) {
           hiddenDate = selectedYear + "-" + selectedMonth + "-" + selectedDay;
-          $(this).find('#'+settings["fieldId"]).val(hiddenDate);
-          if (settings["onChange"] != null) {
-            settings["onChange"](hiddenDate);
+          $(this).find('#'+settings.fieldId).val(hiddenDate);
+          if (settings.onChange !== null) {
+            settings.onChange(hiddenDate);
           }
         }
-      });
+      };
+
+      // If having a legend options
+      if (settings.legend) { $("<legend>" + settings.legends + "</legend>").appendTo($fieldset); }
+
+      var tabindex = settings.tabindex;
+
+      // Deal with the various Date Formats
+      if (settings.dateFormat === "bigEndian") {
+        $fieldset.append($year).append($month).append($day);
+        if (tabindex !== null) {
+          $year.attr('tabindex', tabindex);
+          $month.attr('tabindex', tabindex++);
+          $day.attr('tabindex', tabindex++);
+        }
+      } else if (settings.dateFormat === "littleEndian") {
+        $fieldset.append($day).append($month).append($year);
+        if (tabindex !== null) {
+          $day.attr('tabindex', tabindex);
+          $month.attr('tabindex', tabindex++);
+          $year.attr('tabindex', tabindex++);
+        }
+      } else {
+        $fieldset.append($month).append($day).append($year);
+        if (tabindex !== null) {
+          $month.attr('tabindex', tabindex);
+          $day.attr('tabindex', tabindex++);
+          $year.attr('tabindex', tabindex++);
+        }
+      }
+
+      // Add the option placeholders if specified
+      if (settings.placeholder) {
+        $("<option value='0'>Year:</option>").appendTo($year);
+        $("<option value='0'>Month:</option>").appendTo($month);
+        $("<option value='0'>Day:</option>").appendTo($day);
+      }
+
+      var hiddenDate;
+      if (settings.defaultDate) {
+        var defDate = new Date(settings.defaultDate + "T00:00:00"),
+        defYear = defDate.getFullYear(),
+        defMonth = defDate.getMonth() + 1,
+        defDay = defDate.getDate();
+        hiddenDate = defYear + "-" + defMonth + "-" + defDay;
+      }
+
+      // Create the hidden date markup
+      if (settings.hiddenDate) {
+        $("<input type='hidden' name='" + settings.fieldName + "'/>")
+            .attr("id", settings.fieldId)
+            .val(hiddenDate)
+            .appendTo($fieldset);
+      }
+
+      // Build the initial option sets
+      var startYear = todayYear - settings.minAge;
+      var endYear = todayYear - settings.maxAge;
+      if (settings.futureDates && settings.maxYear !== todayYear) {
+        if (settings.maxYear > 1000) { startYear = settings.maxYear; }
+        else { startYear = todayYear + settings.maxYear; }
+      }
+
+      var i, j, k;
+      for (i=startYear; i>=endYear; i--) { $("<option></option>").attr("value", i).text(i).appendTo($year); }
+      for (j=0; j<12; j++) { $("<option></option>").attr("value", j+1).text(months[settings.monthFormat][j]).appendTo($month); }
+      for (k=1; k<32; k++) { $("<option></option>").attr("value", k).text(k).appendTo($day); }
+      $(this).append($fieldset);
+
+      // Set the default date if given
+      if (settings.defaultDate) {
+        var date = new Date(settings.defaultDate + "T00:00:00");
+        $year.val(date.getFullYear());
+        $month.val(date.getMonth() + 1);
+        $day.val(date.getDate());
+
+        // Update the option immediately after setting the default date
+        updateSelectOptions();
+      }
+
+      // Update the option sets according to options and user selections
+      $fieldset.change(updateSelectOptions);
     });
   };
-})( jQuery );
+})(jQuery);
+
+
